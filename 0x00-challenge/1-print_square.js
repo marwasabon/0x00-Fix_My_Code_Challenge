dcecc9b3845b4e9256a9f7fae82e43c7313dd0b5
marwasabon <marwasabon@gmail.com>
2023-11-21 02:32:49 +0000

    Task 1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -16,6 +16,11 @@ if (process.argv.length <= 2) {
 
 let size = parseInt(process.argv[2])
 
+if ((size)< 0) {
+	process.stdout.write("Invalid size\n");
+	process.exit(1);
+}
+
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
