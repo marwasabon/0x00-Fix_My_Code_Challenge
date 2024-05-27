1f8f75a20da395c47953cb28833ebaa477ae661d
marwasabon <marwasabon@gmail.com>
2024-05-27 10:58:52 +0000

    task 1 fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -27,7 +27,7 @@ class square():
 
 
 if __name__ == "__main__":
-
+    """ Main function creates square object """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
